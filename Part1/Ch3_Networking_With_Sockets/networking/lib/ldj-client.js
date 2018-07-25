7877af319d8c37ac0eba2d00daeefccec29bd603
amitpundeer <amitpundeer039@gmail.com>
2018-07-26 01:51:07 +0530

    Ch3 Client Server - bug fix using nodejs custom modules

---
+++ Part1/Ch3_Networking_With_Sockets/networking/lib/ldj-client.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const EventEmitter = require('events').EventEmitter;
+class LDJClient extends EventEmitter {
+    constructor(stream) {
+        super();
+        
+        let buffer = '';
+
+        stream.on('data', data=> {
+            buffer += data;
+            let boundary = buffer.indexOf('\n');
+            
+            while(boundary !== -1) {
+                // Getting the data from socket object
+                const input = buffer.substring(0, boundary);
+                // Slearing and checking if the next message starts after \n 
+                buffer = buffer.substring(boundary+1);
+                // Emit the event as complete data is recieved in input
+                this.emit('message', JSON.parse(input));
+                // Adding \n to signify the end of prev message and start of new message
+                boundary = buffer.indexOf('\n');
+                }
+            });
+        }
+
+        static connect(stream) {
+            return new LDJClient(stream);
+        }
+}
+
+module.exports = LDJClient;
